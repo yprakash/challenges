69a1a5b643ea2d6ef5afe773286c27fe7da3af33
Prakash Yenugandula <prakash.yenugandula@opsramp.com>
2020-11-03 09:35:12 +0530

    Formatted solution

--- modules/hackerrank/src/main/java/com/yprakash/algorithms/strings/WeightedUniformStrings.java
+++ modules/hackerrank/src/main/java/com/yprakash/algorithms/strings/WeightedUniformStrings.java
@@ -1,6 +1,6 @@
 /**
  * @author Prakash Yenugandula
- * Date  : 2020-11-02
+ * Date  : 2020-11-03
  * https://www.hackerrank.com/challenges/weighted-uniform-string/problem
  */
 
@@ -13,68 +13,68 @@ import java.util.Scanner;
 
 public class WeightedUniformStrings
 {
-    // Complete the weightedUniformStrings function below.
-    static String[] weightedUniformStrings(String s, int[] queries) {
-    	int max = 10000001;
-    	boolean[] res = new boolean[max];
-    	char[] ch = s.toCharArray();
-    	int i, j, n = ch.length;
-    	for(i = 0; i < n; ) {
-    		int x = getInt(ch[i]);
-    		System.out.println(ch[i] + " = " + x);
-    		int sum = 1;
-    		res[x] = true;
-    		for(j = i+1; j < n && ch[i] == ch[j]; j++) {
-    			sum++;
-    			if(sum * x < max) {
-            		res[sum * x] = true;
-    			}
-    		}
-    		i = j;
-    	}
+	// It is 100% accepted solution
+	static String[] weightedUniformStrings(String s, int[] queries) {
+		int max = 10000001;
+		boolean[] res = new boolean[max];
+		char[] ch = s.toCharArray();
+		int i, j, n = ch.length;
+		for(i = 0; i < n; ) {
+			int x = getInt(ch[i]);
+			// System.out.println(ch[i] + " = " + x);
+			int sum = 1;
+			res[x] = true;
+			for(j = i+1; j < n && ch[i] == ch[j]; j++) {
+				sum++;
+				if(sum * x < max) {
+					res[sum * x] = true;
+				}
+			}
+			i = j;
+		}
 
-    	n = queries.length;
-    	String[] res2 = new String[n];
-    	for (i = 0; i < n; i++) {
-    		res2[i] = res[queries[i]] ? "Yes" : "No";
-    	}
-    	return res2;
-    }
+		n = queries.length;
+		String[] res2 = new String[n];
+		for (i = 0; i < n; i++) {
+			res2[i] = res[queries[i]] ? "Yes" : "No";
+		}
+		return res2;
+	}
 
-    private static int getInt(char c) {
+	private static int getInt(char c) {
 		return (int) c - 96;
 	}
 
 	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String s = scanner.nextLine();
+		String s = scanner.nextLine();
 
-        int queriesCount = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int queriesCount = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int[] queries = new int[queriesCount];
+		int[] queries = new int[queriesCount];
 
-        for (int i = 0; i < queriesCount; i++) {
-            int queriesItem = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-            queries[i] = queriesItem;
-        }
+		for (int i = 0; i < queriesCount; i++) {
+			int queriesItem = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+			queries[i] = queriesItem;
+		}
 
-        String[] result = weightedUniformStrings(s, queries);
+		String[] result = weightedUniformStrings(s, queries);
 
-        for (int i = 0; i < result.length; i++) {
-            bufferedWriter.write(result[i]);
+		for (int i = 0; i < result.length; i++) {
+			bufferedWriter.write(result[i]);
 
-            if (i != result.length - 1) {
-                bufferedWriter.write("\n");
-            }
-        }
+			if (i != result.length - 1) {
+				bufferedWriter.write("\n");
+			}
+		}
 
-        bufferedWriter.newLine();
-        bufferedWriter.close();
-        scanner.close();
-    }
+		bufferedWriter.newLine();
+		bufferedWriter.close();
+		scanner.close();
+	}
 }
